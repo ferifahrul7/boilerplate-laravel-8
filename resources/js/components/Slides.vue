dbb5cdb5a765c8a3cc9e71de0444a5b8d9150ad9
ferifahrul7 <ferifahrul1@gmail.com>
2021-07-08 14:51:40 +0700

    hapus yang gk penting

---
+++ resources/js/components/Slides.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="reply in items">
+      <reply :data="reply"></reply>
+    </div>
+  </div>
+</template>
+<script>
+import Slide from "./Slide.vue";
+
+export default {
+  props: ["data"], // accepts data for replies
+
+  components: { Slide },
+
+  data() {
+    return {
+      items: this.data,
+    };
+  },
+};
+</script>

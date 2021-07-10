dbbfdb0f81252a03bf3dcf47965796cdaa16793e
ferifahrul7 <ferifahrul1@gmail.com>
2021-07-10 13:50:23 +0700

    delete seeder dan web yang tidak terpakai

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use App\Models\RoleHierarchy;
+
+class UsersAndNotesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* Create roles */
+        $adminRole = Role::create(['name' => 'admin']); 
+        RoleHierarchy::create([
+            'role_id' => $adminRole->id,
+            'hierarchy' => 1,
+        ]);
+        $userRole = Role::create(['name' => 'user']);
+        RoleHierarchy::create([
+            'role_id' => $userRole->id,
+            'hierarchy' => 2,
+        ]);
+        $guestRole = Role::create(['name' => 'guest']); 
+        RoleHierarchy::create([
+            'role_id' => $guestRole->id,
+            'hierarchy' => 3,
+        ]);
+        
+        /*  insert users   */
+        $user = User::create([ 
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'menuroles' => 'user,admin' 
+        ]);
+        $user->assignRole('admin');
+        $user->assignRole('user');
+        
+    }
+}
